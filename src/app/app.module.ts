3584046cb413c0e7ff266603002e1ed0fb5c18a5
yanpieer-romero-unmsm <yanpieer.romero@unmsm.edu.pe>
2021-11-05 13:39:44 -0500

    Se creó la app-angular-inventory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ProductImageComponent } from "./product-image/product-image.component";
+import { ProductDepartmentComponent } from "./product-department/product-department.component";
+import { PriceDisplayComponent } from "./price-display/price-display.component";
+import { ProductRowComponent } from "./product-row/product-row.component";
+import { ProductsListComponent } from "./products-list/products-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent,
+    ProductRowComponent,
+    ProductsListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,19 @@
+import {
+  Component,
+  Input
+} from '@angular/core';
+
+/**
+ * @PriceDisplay: A component to show the price of a
+ * Product
+ */
+@Component({
+  selector: 'price-display',
+  template: `
+  <div class="price-display">\${{ price }}</div>
+  `
+})
+export class PriceDisplayComponent {
+  @Input()
+  price!: number;
+}
